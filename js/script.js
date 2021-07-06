b7e65906807bcc2df708ff65bb34e67272dcc2db
FrancescoCina <francesco.cina04@gmail.com>
2021-07-06 15:22:30 +0200

    Primo commit: agganciato file js e fatti passaggi esercizio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("JS Ok");
+
+
+/*
+
+Consegna:
+1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+3. stampa la lista ordinata alfabeticamente sia in console che in pagina
+4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+*/
+
+
+/*
+1- costruisco array con i cognomi della consegna;
+2- prompt per il cognome dell'utente;
+3- faccio il push del prompt all'interno dell'array;
+4- applico il metodo sort al mio array;
+5- stampo in console e in pagina;
+6- Modifica la posizione dei cognomi all'interno dell'array;
+
+*/
