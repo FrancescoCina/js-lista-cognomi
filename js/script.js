5d8eeecc8810a1481e0ca8602a9d1f18e0109f03
FrancescoCina <francesco.cina04@gmail.com>
2021-07-06 17:36:01 +0200

    Inserito tag <p> con posizione "umana" del cognome

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-console.log("JS Ok");
-
-
 /*
 
 Consegna:
@@ -20,74 +17,88 @@ Consegna:
 6- Modifica la posizione dei cognomi all'interno dell'array;
 
 */
-
+// PER PRENDERMI L'HTML
 
 var cpuList = document.getElementById("list-cpu-surname");
 var humanList = document.getElementById("list-human-surname");
+var positionSurname = document.getElementById("position-surname");
 
-var listSurname = ["", "Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+// ARRAY COGNOMI
+
+var listSurname = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 console.table(listSurname);
+
+// RICHIESTA COGNOME UTENTE
 
 var userSurname = prompt("Qual è il tuo cognome?");
 // console.log(userSurname);
 
+
+// INSERIMENTO COGNOME UTENTE IN ARRAY CREATO
+
 listSurname.push(userSurname);
 console.table(listSurname);
+
+// ORDINO ARRAY
 
 listSurname.sort();
 console.log("Cognomi messi in ordine a-z");
 console.table(listSurname);
 
+
+
+// STAMPO IN ORDINE ALFABETICO
+
 var listItem = "";
 
-
-
-
+// con for
 /* 
 for (var i = 0; i < listSurname.length; i++) {
     listItem += "<li>" + listSurname[i] + "</li>";
 }  */
 
 
+// con while
 
-
-
-// traduzione del for in while
-
-/* var i = 0;
+var i = 0;
 while (i < listSurname.length) {
     listItem += "<li>" + listSurname[i] + "</li>";
     i++;
 }
 
-cpuList.innerHTML = listItem; 
-
- */
-
+// STAMPA IN CONSOLE
+console.log(listItem);
 
 
-/* var i = 1;
+// STAMPA IN HTML
+cpuList.innerHTML = listItem;
+
+
+
+// CON WHILE
+/* var i = 0;
 while (i < listSurname.length + 1) {
     listItem += "<li>" + i + " " + listSurname[i] + "</li>";
     ++i;
 } */
 
 
-
-for (var i = 1; i < listSurname.length; i++) {
-    listItem += "<li>" + i + " " + listSurname.sort()[i] + "</li>";
+for (var i = 0; i < listSurname.length; i++) {
+    listItem += "<li>" + (i + 1) + " " + listSurname[i] + "</li>";
 }
 
-listItem.slice(1);
-
-console.log("Dopo slice.");
-console.table(listItem);
+// console.table(listItem);
 humanList.innerHTML = listItem;
 
 
+// PER STABILIRE POSIZIONE "UMANA": 
 
-
+var position = listSurname.indexOf(userSurname) + 1;
+positionSurname.innerHTML = "La posizione del numero inserito è: " + position;
+console.log("Posizione cognome inserito: ", listSurname.indexOf(userSurname) + 1);
 
 
 
 // TO DO: .TO LOWER CASE PER LE MAIUSCOLE!!!!!
+// TO DO: Pulisci codice!!!
